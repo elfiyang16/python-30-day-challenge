76017d042cce3da4eb7af6e1c15d28f791a1ffa8
Elfi Yang <elfiyang16@gmail.com>
2020-08-21 19:46:21 +0100

    feat: add fallback

--- web-scrapping/scrape.py
+++ web-scrapping/scrape.py
@@ -17,11 +17,14 @@ def url_to_txt(url, filename="word.html", save=False):
             with open(f"word-{year}.html", "w") as f:
                 f.write(html_text)
         return html_text
-    return ""
+    return None
 
 
 def parse_and_extract(url, name="2020"):
     html_text = url_to_txt(url)
+    # just incase we can't get the document, just return
+    if html_text == None:
+        return False
     # save html to variable
     r_html = HTML(html=html_text)
     table_class = ".imdb-scroll-table"
@@ -29,28 +32,30 @@ def parse_and_extract(url, name="2020"):
     r_table = r_html.find(table_class)
 
     # if we find such element
-    if len(r_table) == 1:
-        # select the first element
-        parsed_table = r_table[0]
-        # get all the trs
-        rows = parsed_table.find("tr")
-        # the first row is the header
-        header_row = rows[0]
-        header_cols = header_row.find("th")
-        # iterate through header th to find the column names
-        header_names = [x.text for x in header_cols]
-        # a holder for tables
-        table_data = []
-        # get all data from each row
-        for row in rows[1:]:
-            cols = row.find("td")
-            row_data = []
-            # iterable
-            for i, col in enumerate(cols):
-                row_data.append(col.text)
-            table_data.append(row_data)
-        # print(header_names)
-        # print(table_data)
+    if len(r_table) == 0:
+        return False
+
+    # select the first element
+    parsed_table = r_table[0]
+    # get all the trs
+    rows = parsed_table.find("tr")
+    # the first row is the header
+    header_row = rows[0]
+    header_cols = header_row.find("th")
+    # iterate through header th to find the column names
+    header_names = [x.text for x in header_cols]
+    # a holder for tables
+    table_data = []
+    # get all data from each row
+    for row in rows[1:]:
+        cols = row.find("td")
+        row_data = []
+        # iterable
+        for i, col in enumerate(cols):
+            row_data.append(col.text)
+        table_data.append(row_data)
+    # print(header_names)
+    # print(table_data)
     # ['Rank', 'Release Group', 'Worldwide', 'Domestic', '%', 'Foreign', '%']
     # [['1', 'Bad Boys for Life', '$424,617,855', '$204,417,855', '48.1%', '$220,200,000', '51.9%'],
     # get it store in file dynamically
@@ -60,6 +65,7 @@ def parse_and_extract(url, name="2020"):
 
     df = pd.DataFrame(table_data, columns=header_names)
     df.to_csv(filepath, index=False)
+    return True
 
 
 def run(start_year=None, years_ago=1):
@@ -73,15 +79,17 @@ def run(start_year=None, years_ago=1):
     #  interate through start to ago years and scrape through
     for i in range(0, years_ago+1):
         url = f"https://www.boxofficemojo.com/year/world/{start_year}/"
-        parse_and_extract(url, name=start_year)
-        print(f"Finished{start_year}")
+        finished = parse_and_extract(url, name=start_year)
+        if finished:
+            print(f"Finished{start_year}")
+        else:
+            print(f"{start_year} not finished ")
         start_year -= 1
 
 
 if __name__ == "__main__":
     #  pass in sys arg as input :
     # python3 -i scrape.py 2015 5
-    start, count = sys.argv[1], sys.argv[2]
     #  if input is mal formated then pass in default input
     try:
         start = int(sys.argv[1])
