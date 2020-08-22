04189a4d71ab85ebe131ef94403e6b54226402dc
Elfi Yang <elfiyang16@gmail.com>
2020-08-22 20:06:17 +0100

    feat: get v4 with auth

--- rest-api/connect.py
+++ rest-api/connect.py
@@ -17,9 +17,28 @@ api_base_url = f"https://api.themoviedb.org/{api_version}"
 endpoint_path = f"/movie/{movie_id}"
 endpoint = f"{api_base_url}{endpoint_path}?api_key={api_key}"
 
-r = requests.get(endpoint)
+# r = requests.get(endpoint)
 
-print(endpoint)
+# print(endpoint)
 
-print(r.status_code)
-print(r.text)
+# print(r.status_code)
+# print(r.text)
+
+
+#  v4
+
+movie_id = 501
+api_version = 4
+api_base_url = f"https://api.themoviedb.org/{api_version}"
+endpoint_path = f"/movie/{movie_id}"
+endpoint = f"{api_base_url}{endpoint_path}"
+headers = {
+    'Authorization': f'Bearer {api_key_v4}',
+    'Content-Type': 'application/json;charset=utf-8'
+}
+# r = requests.get(endpoint, headers=headers) # json={"api_key": api_key})
+# print(r.status_code)
+# print(r.text)
+
+
+#  search
