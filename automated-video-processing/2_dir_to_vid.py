ed83dd9508198520eec33efe6892a33999fa3f31
Elfi Yang <elfiyang16@gmail.com>
2020-08-24 19:06:58 +0100

    feat: magically process image based on different parames

--- automated-video-processing/2_dir_to_vid.py
+++ automated-video-processing/2_dir_to_vid.py
@@ -1,5 +1,5 @@
 from conf import SAMPLE_INPUTS, SAMPLE_OUTPUTS
-from moviepy.editor import *
+from moviepy.editor import *  # ImageClip
 from PIL import Image
 
 import os
@@ -24,6 +24,41 @@ filepaths = [os.path.join(thumbnail_per_half_seond_dir, fname)
 #         path = os.path.join(thumbnail_dir, fname)
 #         filepaths.append(path)
 
-print(filepaths)
-clip = ImageSequenceClip(filepaths, fps=1)  # each image last 1 s
+# RUN:
+# clip = ImageSequenceClip(filepaths, fps=1)  # each image last 1 s
+# clip.write_videofile(output_video)
+
+
+directory = {}
+# generates the file names in a directory tree by walking the tree either top-down or bottom-up.
+for root, dirs, files in os.walk(thumbnail_per_half_seond_dir):
+    for fname in files:
+        filepath = os.path.join(root, fname)
+        # key-value pair for each filet:
+        try:
+            key = float(fname.replace(".jpg", ""))
+        except:
+            key = None
+        if key != None:
+            directory[key] = filepath  # 1000 : ./temp/1000
+
+new_paths = []
+for k in sorted(directory.keys()):
+    filepath = directory[k]
+    new_paths.append(filepath)
+    # print(k)  # 220000.0
+
+# RUN:
+# clip = ImageSequenceClip(new_paths, fps=10)
+# clip.write_videofile(output_video)
+
+#  turn each filepath to a file itself
+my_clips = []
+for path in list(new_paths):
+    frame = ImageClip(path)
+    print(frame.img)  # numpy array
+    my_clips.append(frame.img)
+
+# RUN:
+clip = ImageSequenceClip(my_clips, fps=22)
 clip.write_videofile(output_video)
