b441923f08c92bdf9b3fb7c87dd9f284584c775c
Elfi Yang <elfiyang16@gmail.com>
2020-08-24 18:19:56 +0100

    feat: get image per time we want e.g. 0.5s

--- automated-video-processing/one_thumbs.py
+++ automated-video-processing/one_thumbs.py
@@ -8,9 +8,12 @@ source_path = os.path.join(SAMPLE_INPUTS, "sample.mp4")
 thumbnail_dir = os.path.join(SAMPLE_INPUTS, "thumbnails")
 # make a dir to store the images we got
 thumbnail_per_frame_dir = os.path.join(SAMPLE_INPUTS, "thumbnails-per-frame")
+thumbnail_per_half_seond_dir = os.path.join(
+    SAMPLE_INPUTS, "thumbnails_per_half_seond")
 
 os.makedirs(thumbnail_dir, exist_ok=True)
 os.makedirs(thumbnail_per_frame_dir, exist_ok=True)
+os.makedirs(thumbnail_per_half_seond_dir, exist_ok=True)
 
 clip = VideoFileClip(source_path)
 
@@ -21,17 +24,25 @@ print(clip.duration)
 duration = clip.duration
 max_duration = int(duration) + 1
 # convert to a int
+fps = clip.reader.fps
+nframes = clip. reader.nframes
+seconds = nframes/(fps*1.)
 for i in range(0, max_duration):
     print(f"frame at {i} seconds")
     #  turn the frames into a enumerate
     for i, frame in enumerate(clip.iter_frames()):
         # frame = clip.get_frame(int(i))
-        # print(frame)  # np.array
-        # new_img_filepath = os.path.join(thumbnail_dir, f"{i}.jpg")
-        new_img_filepath = os.path.join(thumbnail_per_frame_dir, f"{i}.jpg")
-        # save image per frame as jpg using Image lib
-        new_image = Image.fromarray(frame)
-        new_image.save(new_img_filepath)
+        if i % fps == 0:
+            current_ms = (i/fps) * 1000
+            # print(frame)  # np.array
+            # new_img_filepath = os.path.join(thumbnail_dir, f"{i}.jpg")
+
+            new_img_filepath = os.path.join(
+                thumbnail_per_frame_dir, f"{i}.jpg")
+            # save image per frame as jpg using Image lib
+            new_image = Image.fromarray(frame)
+            new_image.save(new_img_filepath)
+
 
 """
  [[172 116  77]
@@ -42,3 +53,16 @@ for i in range(0, max_duration):
   [ 55  37  26]
   [ 57  39  28]]]
  """
+for i, frame in enumerate(clip.iter_frames()):
+    # frame = clip.get_frame(int(i))
+    fps_half = int(fps/2.0)
+    if i % fps_half == 0:
+        current_ms = (i/fps_half) * 1000
+        # print(frame)  # np.array
+        # new_img_filepath = os.path.join(thumbnail_dir, f"{i}.jpg")
+
+        new_img_filepath = os.path.join(
+            thumbnail_per_half_seond_dir, f"{i}.jpg")
+        # save image per frame as jpg using Image lib
+        new_image = Image.fromarray(frame)
+        new_image.save(new_img_filepath)
