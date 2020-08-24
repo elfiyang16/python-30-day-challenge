f0a12b10e824247fa2e93edc24423b59d39d0c0c
Elfi Yang <elfiyang16@gmail.com>
2020-08-24 18:53:18 +0100

    feat:conver image to video

--- automated-video-processing/2_dir_to_vid.py
+++ automated-video-processing/2_dir_to_vid.py
@@ -10,13 +10,20 @@ thumbnail_dir = os.path.join(SAMPLE_OUTPUTS, "thumbnails")
 thumbnail_per_frame_dir = os.path.join(SAMPLE_OUTPUTS, "thumbnails-per-frame")
 thumbnail_per_half_seond_dir = os.path.join(
     SAMPLE_OUTPUTS, "thumbnails_per_half_seond")
-output_video = os.path.join(SAMPLE_OUTPUTS, "thumbs.mp4")
+output_video = os.path.join(SAMPLE_OUTPUTS, 'thumbs.mp4')
 
 # list of all files and directories
-this_dir = os.path.listdir(thumbnail_dir)
+this_dir = os.listdir(thumbnail_per_half_seond_dir)
 # get all the filenames ends with jpg and join with thumbnaoil_dir
-filepaths = [os.path.join(thumbnail_dir, fname)
+filepaths = [os.path.join(thumbnail_per_half_seond_dir, fname)
              for fname in this_dir if fname.endswith("jpg")]
 
+# filepaths = []
+# for fname in this_dir:
+#     if fname.endswith("jpg"):
+#         path = os.path.join(thumbnail_dir, fname)
+#         filepaths.append(path)
 
-clip = ImageSequenceClip(thumbnail_dir)
+print(filepaths)
+clip = ImageSequenceClip(filepaths, fps=1)  # each image last 1 s
+clip.write_videofile(output_video)
