9471a011f55b984537be4760bf6bbc0feeb216f2
Elfi Yang <elfiyang16@gmail.com>
2020-08-26 14:23:09 +0100

    feat: show the data in fastapi

---
+++ data-science-pipelin/README.md
@@ -0,0 +1,5 @@
+`jupyter notebook` to open the interactive notebook
+
+`chmod +x run.sh ` in server and then `./run.sh` to examine the data
+
+port on 8000
---
+++ data-science-pipelin/server/main.py
@@ -0,0 +1,23 @@
+
+import os
+import pandas as pd
+from fastapi import FastAPI
+
+BASE_DIR = os.path.dirname(os.path.dirname(
+    os.path.abspath(__file__)))  # if we dont wrap another path dir we will get server as basedir
+CACHE_DIR = os.path.join(BASE_DIR, 'cache')
+dataset = os.path.join(CACHE_DIR, 'movies-box-office-dataset-cleaned.csv')
+
+
+app = FastAPI()
+
+
+@app.get('/')
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/box-office")
+def read_box_office_numbers():
+    df = pd.read_csv(dataset)
+    return df.to_dict("Rank")  # return rank column
