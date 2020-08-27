1be3199e16580a086e5e2a08cd98a24eb5ceb9e9
Elfi Yang <elfiyang16@gmail.com>
2020-08-27 16:30:18 +0100

    feat: input search and click

--- scrape-automate-pwd-protected-app-selenium/google.py
+++ scrape-automate-pwd-protected-app-selenium/google.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+import time
 
 browser = webdriver.Chrome()
 url = "http://google.com"
@@ -9,3 +10,21 @@ browser.get(url)
 <textarea name='??'><textarea>
 <input name="q" type="text">
 """
+
+time.sleep(2)
+name = 'q'
+search_el = browser.find_element_by_name("q") 
+# print(search_el)
+# search_el = browser.find_elements_by_css_selector("h1")
+search_el.send_keys("selenium python") # this will input the search terms
+
+"""
+<input type='submit' />
+<button type='submit' />
+<form></form>
+<input type="submit">
+"""
+submit_btn_el = browser.find_element_by_css_selector("input[type='submit']") #find the submit buttion 
+print(submit_btn_el.get_attribute('name'))
+time.sleep(2)
+submit_btn_el.click()
